d8e1d1da4155c8de295c41e0e50ad1366fe1536a
YEEEHHH-114514 <v47ywbw9s7@privaterelay.appleid.com>
2026-06-07 17:09:57 +0800

    改进了start-server.bat文件

--- start-server.bat
+++ start-server.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d "%~dp0"
 echo Compiling...
-javac -cp "lib\mysql-connector-j-8.0.33.jar" -d backend backend\main.java
+javac -encoding UTF-8 -cp "lib\mysql-connector-j-8.0.33.jar" -d backend backend\main.java
 if errorlevel 1 (
     echo Compile FAILED!
     pause
